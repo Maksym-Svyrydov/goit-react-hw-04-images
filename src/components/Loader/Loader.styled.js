7530474c185d062f2705ba7fedbe10e20b60da8d
Maksym-Svyrydov <110718671+Maksym-Svyrydov@users.noreply.github.com>
2023-03-18 13:18:58 +0200

    fixed spiner

--- src/components/Loader/Loader.styled.js
+++ src/components/Loader/Loader.styled.js
@@ -1,12 +1,4 @@
 import styled from 'styled-components';
 export const LoaderWrapper = styled.div`
-  display: flex;
-  ${'' /* margin-top: 80px; */}
-  ${
-    '' /* margin-left: auto;
-  margin-right: auto; */
-  }
-  position: absolute;
-  justify-content: center;
-  align-items: center;
+  margin: 0 auto;
 `;
